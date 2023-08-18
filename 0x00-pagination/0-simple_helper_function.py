1f260dacf6908d82ccc9b7d56446d565c25d220d
eyoshalom <eyoshalom@yahoo.com>
2023-08-18 08:55:46 -0700

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+fuction file
+'''
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """_summary_
+
+    Args:
+        page (int): _Current Page_
+        page_size (int): _Numbers of items on per page _
+
+    Returns:
+        Tuple[int, int]: _Current items, The end of items_
+    """
+    return ((page_size * (page - 1)), page_size * page)
